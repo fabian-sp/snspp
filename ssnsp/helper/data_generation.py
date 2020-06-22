1a7a3abe3689c976d7cb8b958fa7bb51b8d0cc9b
fabian-sp <fabian.schaipp@hotmail.de>
2020-06-22 15:13:52 +0200

    add gitignore

---
+++ ssnsp/helper/data_generation.py
@@ -0,0 +1,72 @@
+"""
+@author: Fabian Schaipp
+"""
+
+import numpy as np
+
+from .lasso import Norm1, lsq, block_lsq, logistic_loss
+
+def lasso_test(N = 10, n = 20, k = 5, lambda1 = .1, block = False):
+    if block:
+        m = np.random.randint(low = 3, high = 10, size = N)
+    else:
+        m = np.ones(N, dtype = 'int')
+    
+    if not block:
+        A = np.random.randn(N,n)
+    else:
+        A = []
+        for i in np.arange(N):
+            A.append(np.random.randn(m[i], n))    
+        A = np.vstack(A)
+    
+    # standardize
+    A = A - A.mean(axis=0)
+    A = (1/A.std(axis=0)) * A
+    
+    assert max(abs(A.mean(axis=0))) <= 1e-5
+    assert max(abs(A.std(axis=0) - 1)) <= 1e-5
+    
+    # create true solution
+    x = np.random.randn(k) 
+    x = np.concatenate((x, np.zeros(n-k)))
+    np.random.shuffle(x)
+    
+    # create measurements
+    b = A @ x
+    
+    phi = Norm1(lambda1)    
+    if block:
+        f = block_lsq(A, b, m)
+    else:
+        f = lsq(A, b)
+
+    return x, A, b, f, phi
+
+def logreg_test(N = 10, n = 20, k = 5, lambda1 = .1):
+    
+    np.random.seed(1234)
+    
+    A = np.random.randn(N,n)
+    
+    # standardize
+    A = A - A.mean(axis=0)
+    A = (1/A.std(axis=0)) * A
+    
+    assert max(abs(A.mean(axis=0))) <= 1e-5
+    assert max(abs(A.std(axis=0) - 1)) <= 1e-5
+    
+    # create true solution
+    x = np.random.randn(k) 
+    x = np.concatenate((x, np.zeros(n-k)))
+    np.random.shuffle(x)
+    
+    h = np.exp(A@x)
+    odds = h/(1+h)
+    
+    b = (odds >= .5)*2 -1
+    
+    phi = Norm1(lambda1) 
+    f = logistic_loss(A,b)
+    
+    return x, A, b, f, phi
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+"""
+@author: Fabian Schaipp
+"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,73 +9,9 @@ import seaborn as sns
 import time
 from sklearn.linear_model import Lasso, LogisticRegression
 
-from ssnsp.helper.lasso import Norm1, lsq, block_lsq, logistic_loss
+from ssnsp.helper.data_generation import lasso_test, logreg_test
 from ssnsp.solver.opt_problem import problem
 
-def lasso_test(N = 10, n = 20, k = 5, lambda1 = .1, block = False):
-    if block:
-        m = np.random.randint(low = 3, high = 10, size = N)
-    else:
-        m = np.ones(N, dtype = 'int')
-    
-    if not block:
-        A = np.random.randn(N,n)
-    else:
-        A = []
-        for i in np.arange(N):
-            A.append(np.random.randn(m[i], n))    
-        A = np.vstack(A)
-    
-    # standardize
-    A = A - A.mean(axis=0)
-    A = (1/A.std(axis=0)) * A
-    
-    assert max(abs(A.mean(axis=0))) <= 1e-5
-    assert max(abs(A.std(axis=0) - 1)) <= 1e-5
-    
-    # create true solution
-    x = np.random.randn(k) 
-    x = np.concatenate((x, np.zeros(n-k)))
-    np.random.shuffle(x)
-    
-    # create measurements
-    b = A @ x
-    
-    phi = Norm1(lambda1)    
-    if block:
-        f = block_lsq(A, b, m)
-    else:
-        f = lsq(A, b)
-
-    return x, A, b, f, phi
-
-def logreg_test(N = 10, n = 20, k = 5, lambda1 = .1):
-    
-    np.random.seed(1234)
-    
-    A = np.random.randn(N,n)
-    
-    # standardize
-    A = A - A.mean(axis=0)
-    A = (1/A.std(axis=0)) * A
-    
-    assert max(abs(A.mean(axis=0))) <= 1e-5
-    assert max(abs(A.std(axis=0) - 1)) <= 1e-5
-    
-    # create true solution
-    x = np.random.randn(k) 
-    x = np.concatenate((x, np.zeros(n-k)))
-    np.random.shuffle(x)
-    
-    h = np.exp(A@x)
-    odds = h/(1+h)
-    
-    b = (odds >= .5)*2 -1
-    
-    phi = Norm1(lambda1) 
-    f = logistic_loss(A,b)
-    
-    return x, A, b, f, phi
     
 #%% generate data
 
