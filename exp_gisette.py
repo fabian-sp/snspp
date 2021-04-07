ed5f5c83b8e84d156d8e7a2f9ae44124aa6c1984
fabian-sp <fabian.schaipp@hotmail.de>
2021-04-07 14:03:37 +0200

    add svrg to gisette

--- exp_gisette.py
+++ exp_gisette.py
@@ -48,22 +48,20 @@ initialize_solvers(f, phi)
 
 params_saga = {'n_epochs' : 50, 'gamma': 5.5}
 
-params_svrg = {'n_epochs' : 50, 'batch_size': 1, 'gamma': 12.}
-
+params_svrg = {'n_epochs' : 50, 'batch_size': 50, 'gamma': 75.}
 
 params_adagrad = {'n_epochs' : 200, 'batch_size': 240, 'gamma': 0.028}
-
 
 params_ssnsp = {'max_iter' : 60, 'batch_size': 400, 'sample_style': 'fast_increasing', 'alpha_C' : 7.,\
           "reduce_variance": True}
 
 #params_tuner(f, phi, solver = "saga", gamma_range = np.linspace(4,8, 10))
-#params_tuner(f, phi, solver = "svrg", gamma_range = np.linspace(1, 20, 10), batch_range = np.array([1, 10, 100]))
+#params_tuner(f, phi, solver = "svrg", gamma_range = np.linspace(30, 80, 8), batch_range = np.array([50]))
+#params_tuner(f, phi, solver = "svrg", gamma_range = np.linspace(30, 80, 8), batch_range = np.array([100]))
 #params_tuner(f, phi, solver = "adagrad", batch_range = np.array([50, 250, 500]))
 #params_tuner(f, phi, solver = "ssnsp", gamma_range = np.linspace(5,10, 10), batch_range = np.array([200, 400]))
 
 #%% solve with SAGA
-
 
 Q = problem(f, phi, tol = 1e-9, params = params_saga, verbose = True, measure = True)
 
@@ -86,7 +84,7 @@ Q2 = problem(f, phi, tol = 1e-9, params = params_svrg, verbose = True, measure =
 
 Q2.solve(solver = 'svrg')
 
-print(f.eval(Q2.x) +phi.eval(Q2.x))
+print(f.eval(Q2.x)+phi.eval(Q2.x))
 
 #%% solve with SSNSP
 
@@ -118,6 +116,15 @@ for k in range(K):
     Q1_k = problem(f, phi, tol = 1e-9, params = params_adagrad, verbose = True, measure = True)
     Q1_k.solve(solver = 'adagrad')
     allQ1.append(Q1_k)
+
+#%% solve with SVRG (multiple times)
+
+allQ2 = list()
+for k in range(K):
+    
+    Q2_k = problem(f, phi, tol = 1e-9, params = params_svrg, verbose = True, measure = True)
+    Q2_k.solve(solver = 'svrg')
+    allQ2.append(Q2_k)
     
 #%% solve with SSNSP (multiple times, VR)
 
@@ -160,7 +167,9 @@ kwargs = {"psi_star": psi_star, "log_scale": True, "lw": 0.4, "markersize": 3}
 
 plot_multiple(allQ, ax = ax , label = "saga", ls = '--', marker = '<', **kwargs)
 plot_multiple(allQ1, ax = ax , label = "adagrad", ls = '--', marker = '>', **kwargs)
+plot_multiple(allQ2, ax = ax , label = "svrg", ls = '--', marker = '>', **kwargs)
 plot_multiple(allP, ax = ax , label = "ssnsp", **kwargs)
+
 #plot_multiple(allP1, ax = ax , label = "ssnsp_noVR", name = "ssnsp (no VR)", **kwargs)
 
 
@@ -179,13 +188,14 @@ if save:
 
 #%% coeffcient plot
 
-P = allP[-1]
+#P = allP[-1]
 
 fig,ax = plt.subplots(2, 2,  figsize = (7,5))
 Q.plot_path(ax = ax[0,0], xlabel = False)
 Q1.plot_path(ax = ax[0,1], xlabel = False, ylabel = False)
-P.plot_path(ax = ax[1,0])
-P.plot_path(ax = ax[1,1], mean = True, ylabel = False)
+Q2.plot_path(ax = ax[1,0])
+P.plot_path(ax = ax[1,1], ylabel = False)
+#P.plot_path(ax = ax[1,1], mean = True, ylabel = False)
 
 for a in ax.ravel():
     a.set_ylim(-.5,.3)
@@ -207,6 +217,7 @@ kwargs2 = {"A": X_test, "b": y_test}
 all_loss_P = np.vstack([eval_test_set(X = P.info["iterates"], loss = logreg_loss, **kwargs2) for P in allP])
 all_loss_Q = np.vstack([eval_test_set(X = Q.info["iterates"], loss = logreg_loss, **kwargs2) for Q in allQ])
 all_loss_Q1 = np.vstack([eval_test_set(X = Q.info["iterates"], loss = logreg_loss, **kwargs2) for Q in allQ1])
+all_loss_Q2 = np.vstack([eval_test_set(X = Q.info["iterates"], loss = logreg_loss, **kwargs2) for Q in allQ2])
 
 
 #%%
@@ -216,6 +227,7 @@ kwargs = {"log_scale": False, "lw": 1, "markersize": 3}
 
 plot_multiple_error(all_loss_Q, allQ, ax = ax , label = "saga", ls = '--', marker = '<', **kwargs)
 plot_multiple_error(all_loss_Q1, allQ1, ax = ax , label = "adagrad", ls = '--', marker = '>', **kwargs)
+plot_multiple_error(all_loss_Q2, allQ2, ax = ax , label = "svrg", ls = '--', marker = '>', **kwargs)
 plot_multiple_error(all_loss_P, allP, ax = ax , label = "ssnsp", **kwargs)
 
 ax.set_xlim(-.1, 6)
