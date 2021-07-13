34140e950c9e043f0980c02a37bff288b9ca00d9
fabian-sp <fabian.schaipp@hotmail.de>
2021-07-13 16:09:04 +0200

    sido plots for large l1

--- exp_sido.py
+++ exp_sido.py
@@ -57,9 +57,9 @@ params_snspp = {'max_iter' : 400, 'batch_size': 200, 'sample_style': 'fast_incre
 
 #%% params (l1=1e-2)
 
-params_saga = {'n_epochs' : 20, 'alpha': 11.5}
+params_saga = {'n_epochs' : 20, 'alpha': 8.}
 
-params_svrg = {'n_epochs' : 30, 'batch_size': 20, 'alpha': 500.}
+params_svrg = {'n_epochs' : 30, 'batch_size': 200, 'alpha': 500.}
 
 params_adagrad = {'n_epochs' : 50, 'batch_size': 200, 'alpha': 0.06}
 
@@ -67,7 +67,7 @@ params_snspp = {'max_iter' : 100, 'batch_size': 200, 'sample_style': 'fast_incre
                 "reduce_variance": True}
     
 #params_tuner(f, phi, solver = "saga", alpha_range = np.linspace(5,15,10), n_iter = 20)
-#params_tuner(f, phi, solver = "svrg", alpha_range = np.logspace(2.5, 3.5, 10), batch_range = np.array([20, 200]), n_iter = 30)
+#params_tuner(f, phi, solver = "svrg", alpha_range = np.linspace(400, 1000, 10), batch_range = np.array([50, 200]), n_iter = 30)
 #params_tuner(f, phi, solver = "adagrad", batch_range = np.array([20, 200, 500]))
 #params_tuner(f, phi, solver = "snspp", alpha_range = np.linspace(5, 40, 10), batch_range = np.array([20, 200]), n_iter = 80)
 #params_tuner(f, phi, solver = "snspp", alpha_range = np.linspace(1, 5, 10), batch_range = np.array([20]), n_iter = 80)
@@ -81,7 +81,6 @@ Q.solve(solver = 'saga')
 print(f.eval(Q.x) +phi.eval(Q.x))
 
 #%% solve with ADAGRAD
-
 
 Q1 = problem(f, phi, tol = 1e-9, params = params_adagrad, verbose = True, measure = True)
 
@@ -117,7 +116,7 @@ K = 20
 allQ = list()
 for k in range(K):
     
-    Q_k = problem(f, phi, tol = 1e-9, params = params_saga, verbose = True, measure = True)
+    Q_k = problem(f, phi, tol = 1e-16, params = params_saga, verbose = True, measure = True)
     Q_k.solve(solver = 'saga')
     allQ.append(Q_k)
 
@@ -126,7 +125,7 @@ for k in range(K):
 allQ1 = list()
 for k in range(K):
     
-    Q1_k = problem(f, phi, tol = 1e-9, params = params_adagrad, verbose = True, measure = True)
+    Q1_k = problem(f, phi, tol = 1e-16, params = params_adagrad, verbose = True, measure = True)
     Q1_k.solve(solver = 'adagrad')
     allQ1.append(Q1_k)
 
@@ -135,7 +134,7 @@ for k in range(K):
 allQ2 = list()
 for k in range(K):
     
-    Q2_k = problem(f, phi, tol = 1e-9, params = params_svrg, verbose = True, measure = True)
+    Q2_k = problem(f, phi, tol = 1e-16, params = params_svrg, verbose = True, measure = True)
     Q2_k.solve(solver = 'svrg')
     allQ2.append(Q2_k)
     
@@ -144,7 +143,7 @@ for k in range(K):
 allP = list()
 for k in range(K):
     
-    P_k = problem(f, phi, tol = 1e-9, params = params_snspp, verbose = False, measure = True)
+    P_k = problem(f, phi, tol = 1e-16, params = params_snspp, verbose = False, measure = True)
     P_k.solve(solver = 'snspp')
     allP.append(P_k)
 
@@ -161,16 +160,16 @@ fig,ax = plt.subplots(figsize = (4.5, 3.5))
 
 kwargs = {"psi_star": psi_star, "log_scale": True, "lw": 0.4, "markersize": 3}
 
-Q.plot_objective(ax = ax, ls = '--', marker = '<', **kwargs)
-Q1.plot_objective(ax = ax, ls = '-.', marker = '>', **kwargs)
-Q2.plot_objective(ax = ax, ls = '-.', marker = '<', **kwargs)
-P.plot_objective(ax = ax, **kwargs)
+#Q.plot_objective(ax = ax, ls = '--', marker = '<', **kwargs)
+#Q1.plot_objective(ax = ax, ls = '-.', marker = '>', **kwargs)
+#Q2.plot_objective(ax = ax, ls = '-.', marker = '<', **kwargs)
+#P.plot_objective(ax = ax, **kwargs)
 
 
-#plot_multiple(allQ, ax = ax , label = "saga", ls = '--', marker = '<', **kwargs)
-#plot_multiple(allQ1, ax = ax , label = "adagrad", ls = '--', marker = '>', **kwargs)
-#plot_multiple(allQ2, ax = ax , label = "svrg", ls = '--', marker = '>', **kwargs)
-#plot_multiple(allP, ax = ax , label = "snspp", **kwargs)
+plot_multiple(allQ, ax = ax , label = "saga", ls = '--', marker = '<', **kwargs)
+plot_multiple(allQ1, ax = ax , label = "adagrad", ls = '--', marker = '>', **kwargs)
+plot_multiple(allQ2, ax = ax , label = "svrg", ls = '--', marker = '>', **kwargs)
+plot_multiple(allP, ax = ax , label = "snspp", **kwargs)
 
 
 ax.set_xlim(-.1, 6)
@@ -184,7 +183,7 @@ fig.subplots_adjust(top=0.96,
                     wspace=0.2)
 
 if save:
-    fig.savefig(f'data/plots/exp_sido/obj.pdf', dpi = 300)
+    fig.savefig(f'data/plots/exp_sido/l1_{l1}/obj.pdf', dpi = 300)
 
 #%% coeffcient plot
 
@@ -203,7 +202,7 @@ for a in ax.ravel():
 plt.subplots_adjust(hspace = 0.33)
 
 if save:
-    fig.savefig(f'data/plots/exp_sido/coeff.pdf', dpi = 300)
+    fig.savefig(f'data/plots/exp_sido/l1_{l1}/coeff.pdf', dpi = 300)
 
 
 #%% eval test set loss
@@ -232,19 +231,19 @@ fig,ax = plt.subplots(figsize = (4.5, 3.5))
 
 kwargs = {"log_scale": False, "lw": 0.7, "markersize": 3}
 
-plot_test_error(Q, L_Q,  ax = ax,  marker = '<', **kwargs)
-plot_test_error(Q1, L_Q1,  ax = ax,  marker = '<', **kwargs)
-plot_test_error(Q2, L_Q2,  ax = ax,  marker = '<', **kwargs)
-plot_test_error(P, L_P,  ax = ax,  marker = 'o', **kwargs)
+#plot_test_error(Q, L_Q,  ax = ax,  marker = '<', **kwargs)
+#plot_test_error(Q1, L_Q1,  ax = ax,  marker = '<', **kwargs)
+#plot_test_error(Q2, L_Q2,  ax = ax,  marker = '<', **kwargs)
+#plot_test_error(P, L_P,  ax = ax,  marker = 'o', **kwargs)
 
 
-#plot_multiple_error(all_loss_Q, allQ, ax = ax , label = "saga", ls = '--', marker = '<', **kwargs)
-#plot_multiple_error(all_loss_Q1, allQ1, ax = ax , label = "adagrad", ls = '--', marker = '>', **kwargs)
-#plot_multiple_error(all_loss_Q2, allQ2, ax = ax , label = "svrg", ls = '--', marker = '>', **kwargs)
-#plot_multiple_error(all_loss_P, allP, ax = ax , label = "snspp", **kwargs)
+plot_multiple_error(all_loss_Q, allQ, ax = ax , label = "saga", ls = '--', marker = '<', **kwargs)
+plot_multiple_error(all_loss_Q1, allQ1, ax = ax , label = "adagrad", ls = '--', marker = '>', **kwargs)
+plot_multiple_error(all_loss_Q2, allQ2, ax = ax , label = "svrg", ls = '--', marker = '>', **kwargs)
+plot_multiple_error(all_loss_P, allP, ax = ax , label = "snspp", **kwargs)
 
 ax.set_xlim(-.1, 6)
-#ax.set_ylim(all_loss_P.min()-1e-3, all_loss_P.min()+1e-1)
+ax.set_ylim(all_loss_P.min()-1e-3, all_loss_P.min()+1e-2)
 ax.legend(fontsize = 10)
 
 fig.subplots_adjust(top=0.96,
@@ -255,7 +254,7 @@ fig.subplots_adjust(top=0.96,
                     wspace=0.2)
 
 if save:
-    fig.savefig(f'data/plots/exp_sido/error.pdf', dpi = 300)
+    fig.savefig(f'data/plots/exp_sido/l1_{l1}/error.pdf', dpi = 300)
 
 
 
