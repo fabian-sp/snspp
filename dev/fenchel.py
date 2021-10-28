9280af393cd371c23877157fb632690f1159644f
Fabian Schaipp <fabian.schaipp@hotmail.de>
2021-10-28 17:58:31 +0200

    update fenchel

--- dev/fenchel.py
+++ dev/fenchel.py
@@ -13,9 +13,19 @@ sp.init_printing(use_unicode=True)
 z,u = sp.symbols('z,u')
 
 ### PSEUDOHUBER ####
-b,mu = sp.symbols('b,mu') # specific symbols 
-f = mu*sp.sqrt(1+((z-b)/mu)**2)
+b= sp.symbols('b') # specific symbols 
+mu = sp.symbols('mu', positive = True)
+f = mu*sp.sqrt(1+((z-b)/mu)**2) - mu
 uhat = b + (mu*z)/sp.sqrt(1-z**2)
+        
+### HUBER ####
+b= sp.symbols('b') # specific symbols 
+mu = sp.symbols('mu', positive = True)
+f = sp.Piecewise((1/2*(z-b)**2/mu, sp.Abs(z-b) <= mu), (sp.Abs(z-b) -mu/2 ,True))
+uhat = mu*z+b
+
+#uhat = sp.functions.elementary.piecewise.ExprCondPair(mu*z+b, sp.Abs(z)<=1)
+#fast = sp.Piecewise((1/2*mu*z**2+b*z, sp.Abs(z) <= 1),(sp.oo, True))
 
 #%%
 
@@ -31,20 +41,25 @@ def compute_conjugate(f, uhat = None, danskin = True):
         print(f"Number of stationary points found by Sympy: {len(sols)}")
         uhat = sols[0]
         print(f"Chosen uhat: {uhat}")
-        
-    fast = H.subs(u, uhat).simplify() # plug in
     
-    #sp.simplify(fast1-uhat)
+    fast = H.subs(u, uhat).simplify() # plug in
+   
     if danskin:
         fast1 = uhat
         fast2 = uhat.diff(z).simplify()
     else:
         fast1 = fast.diff(z).simplify()
         fast2 = fast1.diff(z).simplify()
+        #sp.simplify(fast1-uhat)
         
     return f1, H, fast, fast1, fast2
 
-f1, H, fast, fast1, fast2 = compute_conjugate(f, uhat = uhat, danskin = True)
+#%% compute conjugate
+
+danskin = True
+uhat = uhat
+
+f1, H, fast, fast1, fast2 = compute_conjugate(f, uhat = uhat, danskin = danskin)
 
 print(sp.latex(fast1))
 
@@ -66,15 +81,15 @@ def plot_fun(g, arg = z, sub0 = dict(), xrange = np.linspace(-5,5,100), ax = Non
 
 #%% plot loss and conjugates
 
-sub0 = {mu:0.5, b:0}
-X = np.linspace(-1,1,1000)
+sub0 = {mu:0.01, b:0}
+X = np.linspace(-2,2,1000)
 
 colors = ['#2C3E50','#E74C3C','#3498DB','#2980B9']
 
 fig, ax = plt.subplots()        
 _ = plot_fun(f, arg = z, sub0 = sub0, xrange = X, ax = ax, label = 'f', c= colors[0])
 _ = plot_fun(fast, arg = z, sub0 = sub0, xrange = X, ax = ax, label = r'$f^\ast$', c=colors[1])
-_ = plot_fun(fast1, arg = z, sub0 = sub0, xrange = X, ax = ax, label = r"$(f^\ast)$", c=colors[2])
+_ = plot_fun(fast1, arg = z, sub0 = sub0, xrange = X, ax = ax, label = r"$(f^\ast)'$", c=colors[2])
 _ = plot_fun(fast2, arg = z, sub0 = sub0, xrange = X, ax = ax, label = r"$(f^\ast)''$", c=colors[3])
 
 ax.set_ylim(-2,2)
@@ -84,7 +99,7 @@ ax.legend()
 #%% plot objective of conjugate problem
 
 sub0 = {mu:0.5, b:0}
-Z = np.linspace(-1,1,20) # range of fixed z values
+Z = np.linspace(-2,2,20) # range of fixed z values
 U = np.linspace(-5,5,100) # xaxis
 pal = sns.color_palette("rocket", len(Z))
 
