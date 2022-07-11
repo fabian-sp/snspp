8909607f2f2c0fab52f435c6bdc1e314e8fc1ae7
Fabian Schaipp <fabian.schaipp@hotmail.de>
2022-07-11 17:31:42 +0200

    initial batch script

---
+++ scripts/exp_batch.py
@@ -0,0 +1,86 @@
+"""
+@author: Fabian Schaipp
+
+Runs experiments for step size selection stability. A setup file is used to specify all the relevant parameters, see `../data/setups` for examples.
+
+"""
+
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+from snspp.solver.opt_problem import problem, color_dict
+from snspp.helper.data_generation import get_mnist
+
+from snspp.experiments.experiment_utils import initialize_solvers
+
+
+f, phi, X_train, y_train, X_test, y_test = get_mnist()
+initialize_solvers(f, phi)
+
+#%%
+
+batch_sizes = [1e-3, 5e-3, 1e-2, 2e-2, 5e-2]
+K = len(batch_sizes)
+
+params_snspp = {'max_iter' : 100, 'sample_style': 'constant', 'alpha' : 1., 'reduce_variance': True}
+
+res = dict()
+
+for b in batch_sizes:
+    
+       
+    params_snspp['batch_size'] = int(f.N*b)
+    print(params_snspp)
+    
+    P = problem(f, phi, tol = 1e-9, params = params_snspp, verbose = False, measure = True)
+    P.solve(solver = 'snspp')
+
+    res[b] = P.info
+    
+    
+#%%
+
+mean_rt = dict()
+
+fig, axs = plt.subplots(1,2,figsize = (7, 3), gridspec_kw=dict(width_ratios=[5,2]))
+
+##############################
+## first ax
+
+ax = axs[0]
+
+colors = sns.light_palette(color_dict['snspp'], K+1, reverse=False)
+colors = sns.cubehelix_palette(K, start=.5, rot=-.75, as_cmap=False)
+
+for j,b in enumerate(batch_sizes):
+    
+    x = np.arange(len(res[b]['sub_runtime']))
+    y = res[b]['sub_runtime']
+    mean_rt[b] = np.mean(y)
+    
+    ax.plot(x,y, c=colors[j], lw=1, marker='o', markersize=5, markevery=(0,10), label=rf"$b/N={b}$ ")
+
+ax.set_yscale('log')
+ax.set_xlabel('Iteration')
+ax.set_ylabel('Subproblem runtime [sec]', fontsize=10)
+ax.legend(fontsize=8)
+
+_ylim = ax.get_ylim()
+
+##############################
+## second ax
+
+ax = axs[1]
+ax.plot(mean_rt.keys(), mean_rt.values(), c='darkgray', lw = 3, marker='p', markersize=8, markeredgecolor='k')
+ax.grid(ls = '-', lw = .5) 
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlabel(r'$b/N$')
+ax.set_ylabel('subproblem runtime/iter ', fontsize=10)
+ax.set_ylim(_ylim)
+
+fig.tight_layout()
+
+
