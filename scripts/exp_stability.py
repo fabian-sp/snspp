ecb8745c106738a001a53b955c053e7d57ab30d0
Fabian Schaipp <fabian.schaipp@hotmail.de>
2022-07-11 13:54:17 +0200

    make stability script dunctional

--- scripts/exp_stability.py
+++ scripts/exp_stability.py
@@ -14,55 +14,79 @@ from snspp.experiments.stability_utils import load_setup, create_instance, compu
 #%%
 
 setup_id = 'mnist1'
-results = dict()
 
-setup = load_setup(setup_id)
-f, phi, A, b = create_instance(setup)
-psi_star, xsol = compute_psi_star(setup, f, phi, A, b)
-x0 = compute_x0(setup, f, phi)
+def run_stability(setup_id, save=False, load=False):
+
+    results = dict()
+    
+    setup = load_setup(setup_id)
+    f, phi, A, b = create_instance(setup)
+    psi_star, xsol = compute_psi_star(setup, f, phi, A, b)
+    x0 = compute_x0(setup, f, phi)
+    
+    #################################################
+    # run 
+    #################################################
+    
+    methods = list(setup["methods"].keys())
+    
+    if not load:
+        for mt in methods:
+            
+            params = setup["methods"][mt]["params"]
+            batch_size_range = setup["methods"][mt]["batch"]
+            step_size_range = create_alpha_range(setup, mt)
+            
+            this_res = do_grid_run(f, phi, step_size_range, batch_size_range = batch_size_range, psi_star = psi_star, \
+                                   psi_tol = setup["psi_tol"], n_rep = setup["n_rep"], solver = setup["methods"][mt]["solver"],\
+                                   solver_params = params, x0 = x0)
+            
+            
+            results.update({mt: this_res})
+            
+        #################################################
+        # store 
+        #################################################
+        if save:
+            np.save('../data/output/exp_stability_'+setup_id+'.npy', results)    
+    
+    # (or load results) 
+    else:
+        results = load_stability_results(setup_id)
+    
+    #################################################
+    # plot
+    #################################################    
+    SIGMA = 1. # plot 2SIGMA band around the mean
+    
+    fig, ax = plt.subplots(figsize = (7,5))
+    
+    for mt in methods:
+        plot_result(results[mt], ax = ax, replace_inf = setup["y_max"], sigma = SIGMA, psi_tol = setup["psi_tol"])
+        
+    
+    annot_y = setup["y_max"] * 0.9 # y value for annotation
+    ax.hlines(annot_y , ax.get_xlim()[0], ax.get_xlim()[1], 'grey', ls = '-')
+    ax.annotate("no convergence", (ax.get_xlim()[0]*1.5, annot_y*1.02), color = "grey", fontsize = 14)
+    
+    ax.set_ylim(0,)
+    
+    if save:
+        fig.savefig('../data/plots/exp_other/stability_'+setup_id+'.pdf', dpi = 300)
+        
+    return 
+
 
 #%%
 
-methods = list(setup["methods"].keys())
+setups = ['mnist1']
 
-for mt in methods:
-    
-    params = setup["methods"][mt]["params"]
-    batch_size_range = setup["methods"][mt]["batch"]
-    step_size_range = create_alpha_range(setup, mt)
-    
-    this_res = do_grid_run(f, phi, step_size_range, batch_size_range = batch_size_range, psi_star = psi_star, \
-                           psi_tol = setup["psi_tol"], n_rep = setup["n_rep"], solver = setup["methods"][mt]["solver"],\
-                           solver_params = params, x0 = x0)
+for _s in setups:
+    print(f"Running stability for {_s} \n \n")
+    run_stability(setup_id=_s, save=True, load=False)
     
     
-    results.update({mt: this_res})
-        
-
-#%% store (or load results)
-
-np.save('../data/output/exp_stability_'+setup_id+'.npy', results)    
-
-#results = load_stability_results(setup_id)
-
-#%% plot runtime (until convergence) vs step size
-save = False
-
-SIGMA = 1. # plot 2SIGMA band around the mean
-
-fig, ax = plt.subplots(figsize = (7,5))
-
-for mt in methods:
-    plot_result(results[mt], ax = ax, replace_inf = setup["y_max"], sigma = SIGMA, psi_tol = setup["psi_tol"])
     
-
-annot_y = setup["y_max"] * 0.9 # y value for annotation
-ax.hlines(annot_y , ax.get_xlim()[0], ax.get_xlim()[1], 'grey', ls = '-')
-ax.annotate("no convergence", (ax.get_xlim()[0]*1.5, annot_y*1.02), color = "grey", fontsize = 14)
-
-ax.set_ylim(0,)
-
-if save:
-    fig.savefig('../data/plots/exp_other/stability_'+setup_id+'.pdf', dpi = 300)
-
-
+    
+    
+    
