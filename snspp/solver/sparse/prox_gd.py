f41309a62cd8ab889c20cb341755041c4f62294f
Fabian Schaipp <fabian.schaipp@hotmail.de>
2022-07-13 17:02:07 +0200

    start svrg

--- snspp/solver/sparse/prox_gd.py
+++ snspp/solver/sparse/prox_gd.py
@@ -9,7 +9,7 @@ import warnings
 from numba.typed import List
 from numba import njit
 
-from scipy.sparse.csr import csr_matrix
+from scipy.sparse.csr import sparse_xi_inner
 
 from ...helper.utils import stop_scikit_saga
 
@@ -72,5 +72,48 @@ def sparse_saga_loop(f, phi, x_t, A, N, tol, alpha, gradients, n_epochs, reg):
     return x_t, x_hist, step_sizes, eta
 
 
+#%%
+@njit()
+def sparse_svrg_loop(f, phi, x_t, A, N, tol, alpha, n_epochs, batch_size, m_iter):
+    
+    # initialize for diagnostics
+    x_hist = List()
+    step_sizes = List()
+    
+    eta = 1e10
+    x_old = x_t
+    
+    S_iter = int(n_epochs*N / (batch_size*m_iter))
+    
+    for s in np.arange(S_iter):
+        
+        if eta < tol:
+            break
+        
+        full_g = sparse_xi_inner(f, A, x_t).reshape(-1)
+        g_tilde = (1/N) * full_g.sum(axis=0)
 
+        
+        for t in np.arange(m_iter):
+            
+            S = np.random.randint(low = 0, high = N, size = batch_size)
+            
+            # compute the gradient
+            v_t = compute_batch_gradient(f, A, x_t, S)
+            g_tilde_S = 
+            g_t = v_t - (1/batch_size) * full_g[S,:].sum(axis=0) + g_tilde
+
+            w_t = x_t - alpha*g_t
+            x_t = phi.prox(w_t, alpha)
+        
+   
+        # stop criterion
+        eta = stop_scikit_saga(x_t, x_old)
+        x_old = x_t
+        # store in each outer iteration
+        x_hist.append(x_t)    
+        step_sizes.append(alpha)    
+
+
+    return x_t, x_hist, step_sizes, eta
 
