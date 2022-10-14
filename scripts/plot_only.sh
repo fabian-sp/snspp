2a446f23fc6b44365a6d080400425b8759fde59e
Fabian Schaipp <fabian.schaipp@hotmail.de>
2022-10-14 23:31:33 +0200

    ne single exp scripts

---
+++ scripts/plot_only.sh
@@ -0,0 +1,14 @@
+#run for permission: chmod u+x plot_only.sh
+#run for execution: ./plot_only.sh
+
+#!/bin/sh
+#source activate opt_env
+
+python exp_mnist.py 1 0 1 
+python exp_gisette.py 1 0 1
+python exp_covtype.py 1 0 1
+python exp_sido.py 1 0 1 1e-3
+python exp_sido.py 1 0 1 1e-2
+python exp_tstudent.py 1 0 1 2
+python exp_tstudent.py 1 0 1 4
+python exp_tstudent.py 1 0 1 1
